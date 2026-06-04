2c58903b4d45848466290618371e532226d12a2d
ganashree1 <165633912+ganashree1@users.noreply.github.com>
2026-06-04 10:56:26 +0530

    Create AI_Insights.py

---
+++ pges/AI_Insights.py
@@ -0,0 +1,20 @@
+st.header("🤖 AI Career Insights")
+
+st.success("""
+Top skills currently demanded:
+
+1. Python
+2. SQL
+3. Machine Learning
+4. AWS
+5. Spark
+""")
+
+st.info("""
+Recommendation:
+
+Learn Python + SQL +
+Machine Learning +
+Cloud Computing
+for maximum opportunities.
+""")
